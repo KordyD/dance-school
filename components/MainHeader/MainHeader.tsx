e9ab21654d7f15a6d793592d9d6554ed8d728dbe
KordyD <84672403+KordyD@users.noreply.github.com>
2023-06-01 19:05:14 +0300

    Make MainHeader

--- components/MainHeader/MainHeader.tsx
+++ components/MainHeader/MainHeader.tsx
@@ -1,11 +1,21 @@
+import Image from 'next/image';
+import shrek from '@/public/shrek.jpeg';
+import styles from './MainHeader.module.scss';
 
 const MainHeader = () => {
   return (
-    <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing.
-        <button>Lorem ipsum dolor sit amet.</button>
-    </div>
-  )
-}
+    <div className={styles.wrapper}>
 
-export default MainHeader
+      <Image className={styles.mainImage} style={{objectFit: 'cover'}} src={shrek} alt='shrek' fill />
+
+      <div className={styles.textWrapper}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      </div>
+
+      <button>Lorem ipsum dolor sit amet.</button>
+      
+    </div>
+  );
+};
+
+export default MainHeader;
